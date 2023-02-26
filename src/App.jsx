14c24e25b717c6bbec0c06814ebc14d67b91b0f4
Robson Carvalho <robson73904@gmail.com>
2023-02-26 07:16:24 -0300

    :memo: global style added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "./styles/global.style";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html{
+    scroll-behavior: smooth;
+  }
+  body{
+    min-height: 100vh;
+    font-family: 'Inter', sans-serif;
+  }
+  body, *{
+    box-sizing: border-box;
+  }
+`;
+
 export const App = () => {
-  return;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 };
