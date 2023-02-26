63d6fc42d7e59ca635be9a84fd67ac272a0f4373
Robson Carvalho <robson73904@gmail.com>
2023-02-26 07:41:34 -0300

    :memo: navbar created in header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,25 @@
 import { Container } from "./styles";
 
 export const Header = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <nav>
+        <h1 className="portfolioName">Portfólio</h1>
+        <ul className="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Sobre mim</a>
+          </li>
+          <li>
+            <a href="#">Experiência</a>
+          </li>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
 };
