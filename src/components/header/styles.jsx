645669a44ede2964c39cbc265303736fd24cc579
Robson Carvalho <robson73904@gmail.com>
2023-02-26 07:54:41 -0300

    :memo: navbar added style

--- src/components/header/styles.jsx
+++ src/components/header/styles.jsx
@@ -1,3 +1,48 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  padding: 0 24px;
+
+  nav {
+    max-width: 1081px;
+    width: 100%;
+    margin: 0 auto;
+    height: auto;
+    padding: 40px 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .portfolioName {
+      font-style: normal;
+      font-weight: 500;
+      font-size: 24px;
+      text-transform: capitalize;
+      line-height: 29px;
+      color: ${({ theme }) => theme.blue};
+    }
+
+    .menu {
+      display: flex;
+      align-items: center;
+      gap: 64px;
+
+      li {
+        a {
+          text-decoration: none;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 18px;
+          line-height: 22px;
+          color: ${({ theme }) => theme.gray_light};
+          transition: 0.3s;
+
+          &:hover {
+            color: ${({ theme }) => theme.light};
+          }
+        }
+      }
+    }
+  }
+`;
