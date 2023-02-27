c354a86d58eb8195e6077bde8521210a017812bd
Robson Carvalho <robson73904@gmail.com>
2023-02-27 10:27:11 -0300

    :memo: main component added in home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 
 import { Header } from "../../components/header";
+import { Main } from "../../components/main";
 
 export const Home = () => {
   return (
     <Container>
       <Header />
+      <Main />
     </Container>
   );
 };
