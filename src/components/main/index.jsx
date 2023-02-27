b6a25e405d2702b9df517d6087f7465cbae168a5
Robson Carvalho <robson73904@gmail.com>
2023-02-27 11:20:13 -0300

    :memo: section home created and button component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,5 +1,27 @@
-import { Container } from "./styles";
+import { Container, Home } from "./styles";
+
+import looperBG from "../../assets/looperBg.svg";
+import imgLinkedin from "../../assets/linkedin.svg";
+import imgGithub from "../../assets/github.svg";
 
 export const Main = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Home>
+        <div className="content">
+          <div className="texts">
+            <span>OLÁ, EU SOU</span>
+            <h2>ROBSON SOUZA</h2>
+            <span>DESENVOLVEDOR FRONT-END.</span>
+          </div>
+          <div className="links"></div>
+        </div>
+        <img
+          className="looperBG"
+          src={looperBG}
+          alt="Desenho futurista geométrico"
+        />
+      </Home>
+    </Container>
+  );
 };
--- src/components/main/styles.jsx
+++ src/components/main/styles.jsx
@@ -1,3 +1,61 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  padding: 0 24px;
+`;
+
+export const Home = styled.section`
+  max-width: 1081px;
+  width: 100%;
+  height: 625px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 60px;
+
+    .texts {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 12px;
+
+      h2 {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 64px;
+        line-height: 77px;
+        text-transform: uppercase;
+        color: ${({ theme }) => theme.blue};
+      }
+
+      span {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 24px;
+        line-height: 29px;
+        text-transform: uppercase;
+        color: ${({ theme }) => theme.light};
+      }
+    }
+
+    .links {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 24px;
+    }
+  }
+
+  .looperBG {
+    position: absolute;
+    right: 0;
+  }
+`;
