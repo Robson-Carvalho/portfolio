7c14db9aab42f8ea23302b733c0fa2bc36ccd2f3
Robson Carvalho <robson73904@gmail.com>
2023-03-02 18:21:30 -0300

    :memo: stylized scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,20 @@ const GlobalStyle = createGlobalStyle`
   body, *{
     box-sizing: border-box;
   }
+
+  body::-webkit-scrollbar {
+  width: 12px;               /* width of the entire scrollbar */
+}
+
+body::-webkit-scrollbar-track {
+  background: #121212;        /* color of the tracking area */
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #00d2df;    /* color of the scroll thumb */
+  border-radius: 20px;       /* roundness of the scroll thumb */
+  border: 3px solid #151515;  /* creates padding around scroll thumb */
+}
 `;
 
 const cssVariables = {
