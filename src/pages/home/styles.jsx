20986a067a43c99f4b48abf38d26b7cfd2468eda
Robson Carvalho <robson73904@gmail.com>
2023-03-04 10:30:41 -0300

    :memo: code updated

--- src/pages/home/styles.jsx
+++ src/pages/home/styles.jsx
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.dark};
   height: 100vh;
+  overflow-x: hidden;
 
   ${({ isMenuVisible }) =>
     isMenuVisible &&
